482c9a2e3e043e700459234262c960a89e27adab
Arash <arash8877@gmail.com>
2025-07-05 12:31:43 +0200

    create reusable button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import "./App.css";
-import ListGroup from "./components/ListGroup";
-import Alert from "./components/Alert";
+
+import Button from "./components/Button";
 
 function App() {
   const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
+
   return (
     <div>
-      <Alert>Hellow <span>Word</span></Alert>
+      <Button onClick={()=> console.log('click')}>My btn</Button>
     </div>
   );
 }
