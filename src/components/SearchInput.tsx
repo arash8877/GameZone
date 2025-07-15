1b609adc0e6165fd865c095d4cdc957519c71181
Arash <arash8877@gmail.com>
2025-07-15 17:47:56 +0200

    ref the search value in the search bar

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,12 +1,26 @@
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 
 const SearchInput = () => {
+  const ref = useRef<HTMLInputElement>(null);
+  // as we have a simple form here it is easier to use a ref to access the input value
+  // rather than using useState and onChange handler
+
   return (
-    <InputGroup>
-      <InputLeftElement children={<BsSearch />} />
-      <Input borderRadius={20} placeholder="Search games..." variant="filled" />;
-    </InputGroup>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        if (ref.current) {
+          console.log("Search Input Value:", ref.current.value);
+        }
+      }}
+    >
+      <InputGroup>
+        <InputLeftElement children={<BsSearch />} />
+        <Input ref={ref} borderRadius={20} placeholder="Search games..." variant="filled" />;
+      </InputGroup>
+    </form>
   );
 };
 
