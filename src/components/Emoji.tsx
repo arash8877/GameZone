acf553c04f482096b149e732f2e65eeb5d634e48
Arash <arash8877@gmail.com>
2025-07-16 15:36:11 +0200

    add emoji to the game cards

--- src/components/Emoji.tsx
+++ src/components/Emoji.tsx
@@ -1,25 +1,22 @@
-import bullsEye from '../assets/bulls-eye.webp';
-import thumbsUp from '../assets/thumbs-up.webp';
-import meh from '../assets/meh.webp';
-import { Image, ImageProps } from '@chakra-ui/react';
+import bullsEye from "../assets/bulls-eye.webp";
+import thumbsUp from "../assets/thumbs-up.webp";
+import meh from "../assets/meh.webp";
+import { Image, ImageProps } from "@chakra-ui/react";
 
 interface EmojiProps {
-    rating: number;
+  rating: number;
 }
 
-const Emoji = ({rating}: EmojiProps) => {
+const Emoji = ({ rating }: EmojiProps) => {
+  if (rating < 3) return null;
 
-    if (rating < 3) return null;
+  const emojiMap: { [key: number]: ImageProps } = {
+    3: { src: meh, alt: "meh", boxSize: "25px" },
+    4: { src: thumbsUp, alt: "recommended", boxSize: "25px" },
+    5: { src: bullsEye, alt: "exceptional", boxSize: "35px" },
+  };
 
-    const emojiMap: { [key: number]: ImageProps } = {
-      3: { src: meh, alt: 'meh', boxSize: '25px' },
-      4: { src: thumbsUp, alt: 'recommended', boxSize: '25px' },
-      5: { src: bullsEye, alt: 'exceptional', boxSize: '35px' },
-    }
+  return <Image {...emojiMap[rating]} marginTop={1} />;
+};
 
-  return (
-     <Image {...emojiMap[rating]} marginTop={1} />
-  )
-}
-
-export default Emoji
+export default Emoji;
