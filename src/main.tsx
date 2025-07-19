f4e58d0e80acf9ae392ea5583c8e927376f80ed6
Arash <arash8877@gmail.com>
2025-07-19 21:25:57 +0200

    setting up React Query

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,20 @@
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import App from "./App";
 import theme from "./theme";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
       <App />
     </ChakraProvider>
   </StrictMode>
