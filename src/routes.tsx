123aac45d716c048da8da2eede30cbee7b2f5137
Arash <arash8877@gmail.com>
2025-07-22 18:22:31 +0200

    create new route GameDetailPage

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Homepage from "./pages/Homepage";
+import GameDetailPage from "./pages/GameDetailPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    children: [{ path: "", element: <Homepage /> }],
+    children: [
+      { path: "", element: <Homepage /> },
+      { path: "games/:id", element: <GameDetailPage /> },
+    ],
   },
 ]);
 
