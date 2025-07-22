0b346c16e7261067944b24c4b015a279181191f1
Arash <arash8877@gmail.com>
2025-07-22 18:19:00 +0200

    replace App.tsx with RouterProvider

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,16 @@ import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "./index.css";
-import App from "./App";
 import theme from "./theme";
-import { FaLaptopHouse } from "react-icons/fa";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       retry: 3,
       gcTime: 300000, // Garbage Collection Time -  After 5 minutes of no component using the query, the cached data will be deleted.
-      staleTime: 10* 1000, // After 10 seconds of no component using the query, the data will be considered stale and will be refetched on next use.
+      staleTime: 10 * 1000, // After 10 seconds of no component using the query, the data will be considered stale and will be refetched on next use.
       refetchOnReconnect: true,
       refetchOnMount: false,
     },
@@ -25,8 +25,8 @@ createRoot(document.getElementById("root")!).render(
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <QueryClientProvider client={queryClient}>
-        <App />
-        <ReactQueryDevtools initialIsOpen={false}/>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </ChakraProvider>
   </StrictMode>
