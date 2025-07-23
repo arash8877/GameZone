9ed59ebaffbf3a699e2d2e42bf4e72dcc5d2ee50
Arash <arash8877@gmail.com>
2025-07-23 16:50:32 +0200

    modify expandableText component

--- src/components/ExpandableText.tsx
+++ src/components/ExpandableText.tsx
@@ -8,14 +8,25 @@ interface iProps {
 const ExpandableText = ({ children }: iProps) => {
   const [expanded, setExpanded] = useState(false);
 
+  if (!children) return null;
+
   const limit = 300;
   if (children.length <= limit) return <Text>{children}</Text>;
 
-  const summary = children.substring(0, limit);
+  const summary = expanded ? children : children.substring(0, limit) + " ...";
 
   return (
     <Text>
-      {summary}... <Button>{expanded ? "Show Less" : "Show More"}</Button>
+      {summary}
+      <Button
+        size="xs"
+        fontWeight="bold"
+        colorScheme="yellow"
+        marginLeft={1}
+        onClick={() => setExpanded(!expanded)}
+      >
+        {expanded ? "Show Less" : "Show More"}
+      </Button>
     </Text>
   );
 };
